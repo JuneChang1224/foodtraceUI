b56bab2ae427eeec7dd6b2c1bd8012ba69ee980f
JuneChang1224 <yjchang1224@gmail.com>
2025-08-05 12:09:38 +0800

    update page.tsx
    delete unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,18 @@
 // // src/app/page.tsx
 
-// 'use client';
-
-// import React from 'react';
-// import { Header } from './components/Header';
-// import { Footer } from './components/Footer';
-
-// export default function Home() {
-//   return (
-//     <>
-//       <div className="flex flex-col min-h-screen">
-//         <Header />
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-
 'use client';
 
 import React from 'react';
-import { Header } from './components/Header';
+// import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-// import ProductForm from './components/ProductForm';
 import LoginPage from '@/app/login/page';
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <main>
-        {/* <ProductForm /> */} <LoginPage />
+        <LoginPage />
       </main>
       <Footer />
     </div>
